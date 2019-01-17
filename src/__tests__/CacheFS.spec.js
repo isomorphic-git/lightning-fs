587c741901cfe2445bb32d7d35c474b68dbd99e8
William Hilton <wmhilton@gmail.com>
2019-01-17 13:56:26 -0500

    fix: fix awful inode autoincrement bug

--- src/__tests__/CacheFS.spec.js
+++ src/__tests__/CacheFS.spec.js
@@ -1,19 +1,39 @@
 import CacheFS from "../CacheFS";
 
-const fs = new CacheFS();
-
 const treeText = require('./__fixtures__/tree.txt.js');
 
 describe("CacheFS module", () => {
   it("print ∘ parse == id", () => {
+    const fs = new CacheFS();
     let parsed = fs.parse(treeText)
     let text = fs.print(parsed)
     expect(text).toEqual(treeText)
   });
   it("size()", () => {
+    const fs = new CacheFS();
     expect(fs.size()).toEqual(0)
     fs.loadSuperBlock(treeText)
     let inodeCount = treeText.trim().split('\n').length
     expect(fs.size()).toEqual(inodeCount)
+  });
+  it("autoinc()", () => {
+    const fs = new CacheFS();
+    expect(fs.autoinc()).toEqual(1)
+    fs.writeFile('/foo', 'bar', {})
+    expect(fs.autoinc()).toEqual(2)
+    fs.mkdir('/bar', {})
+    expect(fs.autoinc()).toEqual(3)
+    fs.unlink('/foo')
+    expect(fs.autoinc()).toEqual(3)
+    fs.mkdir('/bar/baz', {})
+    expect(fs.autoinc()).toEqual(4)
+    fs.rmdir('/bar/baz')
+    expect(fs.autoinc()).toEqual(3)
+    fs.mkdir('/bar/bar', {})
+    expect(fs.autoinc()).toEqual(4)
+    fs.writeFile('/bar/bar/boo', 'bar', {})
+    expect(fs.autoinc()).toEqual(5)
+    fs.unlink('/bar/bar/boo')
+    expect(fs.autoinc()).toEqual(4)
   });
 });
