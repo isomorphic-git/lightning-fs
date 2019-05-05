eef9ad41d6fea81c1de97a305c322a81b0ab7a4e
Jakub T. Jankiewicz <jcubic@onet.pl>
2019-05-05 15:31:56 +0200

    fix: ignore trailing slash (#14)

--- src/path.js
+++ src/path.js
@@ -19,6 +19,9 @@ function splitPath(path) {
   if (path.length === 0) return [];
   if (path === "/") return ["/"];
   let parts = path.split("/");
+  if (parts[parts.length - 1] === '') {
+      parts.pop();
+  }
   if (path[0] === "/") {
     // assert(parts[0] === '')
     parts[0] = "/";
