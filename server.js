5229f4990f4024c6a15f702447b1b8fb12220ef6
William Hilton <wmhilton@gmail.com>
2019-09-20 15:45:20 -0400

    added 2 more different ways of communicating, kinda messy

--- server.js
+++ server.js
@@ -3,8 +3,10 @@
 const port = 8542
 const WebSocket = require('ws')
 const fs = require('fs').promises
+const { createWriteStream, createReadStream, writeFileSync } = require('fs')
 const path = require('path').posix
 const osa = require('osa')
+const http = require('http')
 
 var promptForPermission = function (appName, dir) {
   var app = Application.currentApplication();
@@ -31,10 +33,6 @@ Only grant access if you expected this website to request this access.`;
   }
 };
 
-const origins = {}
-
-const wss = new WebSocket.Server({ port })
-
 const checkScope = async (origin, dir) => new Promise((resolve, reject) => {
   dir = path.normalize(path.resolve(dir))
 
@@ -44,6 +42,7 @@ const checkScope = async (origin, dir) => new Promise((resolve, reject) => {
   }
   osa(promptForPermission, origin, dir, function (err, result, _log) {
     if (err) {
+      console.log(err)
       return reject(new Error('Access denied'))
     } else {
       if (!result) return reject(new Error('Access denied'))
@@ -62,27 +61,113 @@ const checkMethod = (method) => {
   }
 }
 
+const handleOptions = (req, res) => {
+  res.headers['access-control-allow-origin'] = '*'
+  res.statusCode = 200
+  res.end()
+}
+
+const handlePost = async (req, res) => {
+  // sanitize path argument
+  const rawfilepath = decodeURI(req.url)
+  const filepath = path.normalize(path.resolve(rawfilepath))
+  res.setHeader('access-control-allow-origin', '*')
+  try {
+    await checkScope(req.headers.origin || req.headers['x-origin'], filepath)
+  } catch (e) {
+    res.statusCode = 403
+    res.write(e.message)
+    res.end()
+  }
+  try {
+    const fstream = createWriteStream(filepath)
+    req.pipe(fstream).on('finish', () => {
+      res.statusCode = 201
+      res.end()
+    })
+  } catch (e) {
+    res.statusCode = 500
+    res.write(e.message)
+    res.end()
+  }
+}
+
+const handleGet = async (req, res) => {
+  // sanitize path argument
+  const rawfilepath = decodeURI(req.url)
+  const filepath = path.normalize(path.resolve(rawfilepath))
+  res.setHeader('access-control-allow-origin', '*')
+  try {
+    await checkScope(req.headers.origin || req.headers['x-origin'], filepath)
+  } catch (e) {
+    res.statusCode = 403
+    res.write(e.message)
+    return res.end()
+  }
+  try {
+    await fs.stat(filepath)
+  } catch (e) {
+    res.statusCode = 404
+    return res.end()
+  }
+  const fstream = createReadStream(filepath)
+  fstream.pipe(res) 
+}
+
+const origins = {}
+const wss = new WebSocket.Server({ noServer: true, maxPayload: 0 })
+const server = http.createServer((req, res) => {
+  if (req.method === 'OPTIONS') {
+    return handleOptions(resq, res)
+  }
+  if (req.method === 'POST') {
+    return handlePost(req, res)
+  }
+  if (req.method === 'GET') {
+    return handleGet(req, res)
+  }
+  res.statusCode = 400
+  res.end()
+})
+server.on('upgrade', (request, socket, head) => {
+  wss.handleUpgrade(request, socket, head, (ws) => {
+    wss.emit('connection', ws, request)
+  })
+})
+
 wss.on('connection', (ws, req) => {
   const origin = req.headers.origin
+  let _nextMessageIsBufferWrite = void 0
   ws.on('message', async (data) => {
+    if (_nextMessageIsBufferWrite) {
+      const [id, filepath, opts] = _nextMessageIsBufferWrite
+      _nextMessageIsBufferWrite = void 0
+      try {
+        let res = await fs.writeFile(filepath, data, opts)
+        ws.send(JSON.stringify([-id, res]))
+      } catch (err) {
+        ws.send(JSON.stringify([-id, null, [err.code, err.message]]))
+      }
+      return
+    }
     let [id, method, ...args] = JSON.parse(data)
     try {
-      console.log('rx:', id, method, args[0], '...')
-      // sanitize method
-      checkMethod(method)
       // sanitize path argument
       args[0] = path.normalize(path.resolve(args[0]))
+      // Handle buffers
+      if (method === 'writeFile') {
+        if (args[1] && args[1].nextMessageIsBuffer) {
+          _nextMessageIsBufferWrite = [id, args[0], args[2]]
+          return
+        }
+      }
+      // sanitize method
+      checkMethod(method)
       await checkScope(origin, args[0])
       // sanitize second path argument
       if (method === 'rename' || method === 'symlink') {
         args[1] = path.normalize(path.resolve(args[1]))
         await checkScope(origin, args[1])
-      }
-      // convert JSON to Buffer
-      if (method === 'writeFile') {
-        if (args[1] && args[1].type && args[1].type === 'Buffer') {
-          args[1] = new Uint8Array(args[1].data)
-        }
       }
       let res = await fs[method](...args)
       // convert Stat objects to JSON
@@ -105,9 +190,16 @@ wss.on('connection', (ws, req) => {
           dev: 1
         }
       }
+      if (Buffer.isBuffer(res)) {
+        ws.send(JSON.stringify([-id, { nextMessageIsBuffer: true }]))
+        ws.send(res.buffer)
+        return
+      }
       ws.send(JSON.stringify([-id, res]))
     } catch (err) {
       ws.send(JSON.stringify([-id, null, [err.code, err.message]]))
     }
   })
 })
+
+server.listen(port)
--- src/WebSocketFS.js
+++ src/WebSocketFS.js
@@ -26,10 +26,17 @@ module.exports = class WebSocketFS {
     if (this._activating) return this._activating
     this._activating = new Promise((resolve, reject) => {
       this.socket = new WebSocket(this._url)
+      this.socket.binaryType = 'arraybuffer'
       this.socket.addEventListener('open', () => {
         resolve()
       })
+      let _nextMessageIsBufferCallback = void 0
       this.socket.addEventListener('message', (event) => {
+        if (_nextMessageIsBufferCallback) {
+          const { resolve } = _nextMessageIsBufferCallback
+          _nextMessageIsBufferCallback = void 0
+          return resolve(event.data)
+        }
         let vals = JSON.parse(event.data)
         let [id, retVal, errVal] = vals
         if (id < 0) {
@@ -42,7 +49,11 @@ module.exports = class WebSocketFS {
             cb.e.message = message
             cb.reject(cb.e)
           } else {
-            this._callbacks.get(-id).resolve(retVal)
+            if (retVal && retVal.nextMessageIsBuffer) {
+              _nextMessageIsBufferCallback = this._callbacks.get(-id)
+            } else {
+              this._callbacks.get(-id).resolve(retVal)
+            }
           }
         }
       })
@@ -57,11 +68,17 @@ module.exports = class WebSocketFS {
   }
   async call (method, ...args) {
     await this.activate()
-    this.socket.send(JSON.stringify([this._cbid, method, ...args]))
+    if (method === 'writeFile' && typeof args[1] !== 'string') {
+      // send as buffer
+      this.socket.send(JSON.stringify([this._cbid, method, args[0], { nextMessageIsBuffer: true }, args[2]]))
+      this.socket.send(args[1])
+    } else {
+      this.socket.send(JSON.stringify([this._cbid, method, ...args]))
+    }
     return new Promise((resolve, reject) => {
       let e = new Error()
       this._callbacks.set(this._cbid, { resolve, reject, e })
-      this._cbid++
+      this._cbid = (this._cbid + 1) % Number.MAX_SAFE_INTEGER
     })
   }
   async mkdir(...args) {
@@ -74,13 +91,30 @@ module.exports = class WebSocketFS {
     return this.call('readdir', ...args)
   }
   async writeFile(...args) {
-    return this.call('writeFile', ...args)
+    // if (args[1] && args[1].length > 20000) {
+    //   return this.bigWrite(args[0], args[1])
+    // } else {
+      return this.call('writeFile', ...args)
+    // }
+  }
+  async bigWrite(path, body) {
+    let dest = this._url.replace('ws://', 'http://') + path
+    let res = await fetch(dest, { method: 'POST', body })
+    if (res.status !== 201) throw new Error(res.statusText)
   }
   async readFile(...args) {
+    // return this.bigRead(...args)
     let data = await this.call('readFile', ...args)
     if (data && data.type && data.type === 'Buffer') {
       data = new Uint8Array(data.data)
     }
+    return data
+  }
+  async bigRead(path, opts = {}) {
+    let dest = this._url.replace('ws://', 'http://') + path
+    let res = await fetch(dest, { method: 'GET' })
+    if (res.status !== 200) throw new Error(res.statusText)
+    const data = opts.encoding === 'utf8' ? await res.text() : await res.arrayBuffer()
     return data
   }
   async unlink(...args) {
