27e7ba451ab7bdd5db7c097b277da2481b1e717d
raldone01 <raldone01@gmail.com>
2020-07-17 16:14:45 +0200

    Moved PromisifiedFS into the FS namespace.

--- index.d.ts
+++ index.d.ts
@@ -1,96 +1,4 @@
 declare module '@isomorphic-git/lightning-fs' {
-  export class PromisifiedFS {
-    /**
-     *
-     * @param name This is used to determine the IndexedDb store name.
-     * @param options The "filesystem" configuration.
-     */
-    init(name: string, options?: FS.Options): void
-
-    /**
-     * Make directory
-     * @param filepath
-     * @param options
-     */
-    mkdir(filepath: string, options?: FS.MKDirOptions): Promise<void>
-
-    /**
-     * Remove directory
-     * @param filepath
-     * @param options
-     */
-    rmdir(filepath: string, options?: undefined): Promise<void>
-
-    /**
-     * Read directory
-     *
-     * The Promise return value is an Array of strings. NOTE: To save time, it is NOT SORTED. (Fun fact: Node.js' readdir output is not guaranteed to be sorted either. I learned that the hard way.)
-     * @param filepath
-     * @param options
-     * @returns The file list.
-     */
-    readdir(filepath: string, options?: undefined): Promise<string[]>
-
-    writeFile(filepath: string, data: Uint8Array | string, options?: FS.WriteFileOptions | string): Promise<void>
-
-    readFile(filepath: string, options?: FS.ReadFileOptions | string): Promise<Uint8Array>
-
-    /**
-     * Delete a file
-     * @param filepath
-     * @param options
-     */
-    unlink(filepath: string, options?: undefined): Promise<void>
-
-    /**
-     * Rename a file or directory
-     * @param oldFilepath
-     * @param newFilepath
-     */
-    rename(oldFilepath: string, newFilepath: string): Promise<void>
-
-    /**
-     * The result is a Stat object similar to the one used by Node but with fewer and slightly different properties and methods.
-     * @param filepath
-     * @param options
-     */
-    stat(filepath: string, options?: undefined): Promise<FS.Stats>
-
-    /**
-     * Like fs.stat except that paths to symlinks return the symlink stats not the file stats of the symlink's target.
-     * @param filepath
-     * @param options
-     */
-    lstat(filepath: string, options?: undefined): Promise<FS.Stats>
-
-    /**
-     * Create a symlink at filepath that points to target.
-     * @param target
-     * @param filepath
-     */
-    symlink(target: string, filepath: string): Promise<void>
-
-    /**
-     * Read the target of a symlink.
-     * @param filepath
-     * @param options
-     * @returns The link string.
-     */
-    readlink(filepath: string, options?: undefined): Promise<string>
-
-    /**
-     * Create or change the stat data for a file backed by HTTP. Size is fetched with a HEAD request. Useful when using an HTTP backend without urlauto set, as then files will only be readable if they have stat data. Note that stat data is made automatically from the file /.superblock.txt if found on the server. /.superblock.txt can be generated or updated with the included [standalone script](https://github.com/isomorphic-git/lightning-fs/blob/main/src/superblocktxt.js).
-     * @param filepath
-     * @param options
-     */
-    backFile(filepath: string, options?: FS.BackFileOptions): Promise<void>
-
-    /**
-     * @param filepath
-     * @returns The size of a file or directory in bytes.
-     */
-    du(filepath: string): Promise<number>
-  }
   class FS {
     /**
      * You can procrastinate initializing the FS object until later. And, if you're really adventurous, you can re-initialize it with a different name to switch between IndexedDb databases.
@@ -204,9 +112,102 @@ declare module '@isomorphic-git/lightning-fs' {
      */
     du(filepath: string, cb: (err: Error, size: number) => void): void
 
-    readonly promises: PromisifiedFS
+    readonly promises: FS.PromisifiedFS
   }
   namespace FS {
+    export class PromisifiedFS {
+      /**
+       *
+       * @param name This is used to determine the IndexedDb store name.
+       * @param options The "filesystem" configuration.
+       */
+      init(name: string, options?: FS.Options): void
+
+      /**
+       * Make directory
+       * @param filepath
+       * @param options
+       */
+      mkdir(filepath: string, options?: FS.MKDirOptions): Promise<void>
+
+      /**
+       * Remove directory
+       * @param filepath
+       * @param options
+       */
+      rmdir(filepath: string, options?: undefined): Promise<void>
+
+      /**
+       * Read directory
+       *
+       * The Promise return value is an Array of strings. NOTE: To save time, it is NOT SORTED. (Fun fact: Node.js' readdir output is not guaranteed to be sorted either. I learned that the hard way.)
+       * @param filepath
+       * @param options
+       * @returns The file list.
+       */
+      readdir(filepath: string, options?: undefined): Promise<string[]>
+
+      writeFile(filepath: string, data: Uint8Array | string, options?: FS.WriteFileOptions | string): Promise<void>
+
+      readFile(filepath: string, options?: FS.ReadFileOptions | string): Promise<Uint8Array>
+
+      /**
+       * Delete a file
+       * @param filepath
+       * @param options
+       */
+      unlink(filepath: string, options?: undefined): Promise<void>
+
+      /**
+       * Rename a file or directory
+       * @param oldFilepath
+       * @param newFilepath
+       */
+      rename(oldFilepath: string, newFilepath: string): Promise<void>
+
+      /**
+       * The result is a Stat object similar to the one used by Node but with fewer and slightly different properties and methods.
+       * @param filepath
+       * @param options
+       */
+      stat(filepath: string, options?: undefined): Promise<FS.Stats>
+
+      /**
+       * Like fs.stat except that paths to symlinks return the symlink stats not the file stats of the symlink's target.
+       * @param filepath
+       * @param options
+       */
+      lstat(filepath: string, options?: undefined): Promise<FS.Stats>
+
+      /**
+       * Create a symlink at filepath that points to target.
+       * @param target
+       * @param filepath
+       */
+      symlink(target: string, filepath: string): Promise<void>
+
+      /**
+       * Read the target of a symlink.
+       * @param filepath
+       * @param options
+       * @returns The link string.
+       */
+      readlink(filepath: string, options?: undefined): Promise<string>
+
+      /**
+       * Create or change the stat data for a file backed by HTTP. Size is fetched with a HEAD request. Useful when using an HTTP backend without urlauto set, as then files will only be readable if they have stat data. Note that stat data is made automatically from the file /.superblock.txt if found on the server. /.superblock.txt can be generated or updated with the included [standalone script](https://github.com/isomorphic-git/lightning-fs/blob/main/src/superblocktxt.js).
+       * @param filepath
+       * @param options
+       */
+      backFile(filepath: string, options?: FS.BackFileOptions): Promise<void>
+
+      /**
+       * @param filepath
+       * @returns The size of a file or directory in bytes.
+       */
+      du(filepath: string): Promise<number>
+    }
+
     export interface Options {
       /**
        * Delete the database and start with an empty filesystem
