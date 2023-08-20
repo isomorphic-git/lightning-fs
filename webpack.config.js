b7728753986d941d133721ac6a6b315e5f240084
Jakub Jankiewicz <jcubic@onet.pl>
2023-08-21 00:11:44 +0200

    deps: update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -2,6 +2,7 @@ const path = require('path')
 
 module.exports = {
   target: "webworker",
+  mode: 'production',
   output: {
     path: path.resolve(__dirname, "dist"),
     filename: "lightning-fs.min.js",
